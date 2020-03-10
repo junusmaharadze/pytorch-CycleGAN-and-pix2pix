5b1f8bbee3941f06dad2c4e7d0b103c52ed43b5e
bossemel <18574762+bossemel@users.noreply.github.com>
2020-03-10 22:21:10 +0000

    changed env

--- scripts/cluster_satellite.sh
+++ scripts/cluster_satellite.sh
@@ -37,7 +37,8 @@ rsync -ua /home/${STUDENT_ID}/pytorch-CycleGAN-and-pix2pix/tars/AB.tar.gz ${DATA
 echo $DATASET_DIR
 tar -xzf ${DATASET_DIR}/AB.tar.gz -C ${DATASET_DIR}
 
-source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
+source .env/bin/activate
+#source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 
 sh /home/${STUDENT_ID}/pytorch-CycleGAN-and-pix2pix/scripts/pix2pix_satellite_cluster.sh ${DATASET_DIR}
 
