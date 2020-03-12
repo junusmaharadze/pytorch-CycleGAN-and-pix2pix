034b9774109b9e2ca17fb7b106c1b294bd049f10
bossemel <18574762+bossemel@users.noreply.github.com>
2020-03-12 21:29:43 +0000

    changes for test.py

--- test.py
+++ test.py
@@ -60,9 +60,11 @@ if __name__ == '__main__':
         if i >= opt.num_test:  # only apply our model to opt.num_test images.
             break
         model.set_input(data)  # unpack data from data loader
+        print(model['true_label'])
         model.test()           # run inference
         visuals = model.get_current_visuals()  # get image results
         img_path = model.get_image_paths()     # get image paths
+        labels = model.get_get_image_labels()
         if i % 5 == 0:  # save images to an HTML file
             print('processing (%04d)-th image... %s' % (i, img_path))
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
