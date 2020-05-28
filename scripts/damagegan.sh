34b3e6e42af56794cb40dc39576fba89aadcaa5e
junusmaharadze <junus.maharadze@gmail.com>
2020-05-28 11:31:10 +0200

    added gan training bash script and converted python to python3 in preprocessing

---
+++ scripts/damagegan.sh
@@ -0,0 +1,18 @@
+set -ex
+python train.py \
+--dataroot ./datasets/xBD_datasets/xBD_polygons_AB/AB \
+--labels_file ./datasets/xBD_datasets/xBD_polygons_AB_csv/satellite_AB_labels.txt \
+--name DamageGAN_initial \
+--model pix2pix \
+--direction AtoB \
+--input_nc 3 \
+--output_nc 3 \
+--batch_size 128 \
+--n_epochs 100 \
+--epoch_count 10 \
+--n_epochs_decay 100 \
+--model pix2pix \
+--netG unet_128 \
+--load_size 142 \
+--crop_size 128 \
+--num_threads 1
