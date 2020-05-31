acf243af3d44d7941ea8fea9d378a07443b4967f
marinapts <potsimarina@gmail.com>
2020-05-31 23:24:31 +0100

    Pass arguments to run_classifier.sh

--- scripts/run_classifier.sh
+++ scripts/run_classifier.sh
@@ -12,26 +12,32 @@ fi
 echo "Mode=$mode"
 # echo
 
+MODEL=resnet18
+DATA_DIR=./datasets/xBD_datasets/xBD_polygons_AB
+LABELS_FILE=./datasets/xBD_datasets/xBD_polygons_AB_csv/satellite_AB_labels.txt
+CHECKPOINT_NAME=resnet18_checkpoint
+BATCH_SIZE=16
+
 if [ "$mode" == "train" ]
 then
     set -ex
     python classifiers/train.py \
-    --model resnet18 \
-    --data_dir ./datasets/xBD_datasets/xBD_polygons_AB \
-    --labels_file ./datasets/xBD_datasets/xBD_polygons_AB_csv/satellite_AB_labels.txt \
+    --model $MODEL \
+    --data_dir $DATA_DIR \
+    --labels_file $LABELS_FILE \
     --num_epochs 100 \
-    --batch_size 16 \
-    --checkpoint_name resnet18_checkpoint
+    --batch_size $BATCH_SIZE \
+    --checkpoint_name $CHECKPOINT_NAME
 
 elif [ "$mode" == "test" ]
 then
     set -ex
     python classifiers/test.py \
-    --model resnet18 \
-    --data_dir ./datasets/xBD_datasets/xBD_polygons_AB \
-    --labels_file ./datasets/xBD_datasets/xBD_polygons_AB_csv/satellite_AB_labels.txt \
-    --batch_size 16 \
-    --checkpoint_name resnet18_checkpoint
+    --model $MODEL \
+    --data_dir $DATA_DIR \
+    --labels_file $LABELS_FILE \
+    --batch_size $BATCH_SIZE \
+    --checkpoint_name $CHECKPOINT_NAME
 
 else
     echo "Invalid argument. Use train or test"
