d3b39922d015f9669f4b8a2a6c15c49bd2b1e76d
marinapts <potsimarina@gmail.com>
2020-05-31 15:04:31 +0100

    Add bash script to train classifier with all necessary options

---
+++ scripts/train_classifier.sh
@@ -0,0 +1,6 @@
+set -ex
+python classifiers/train.py \
+--model resnet18 \
+--num_epochs 100 \
+--data_dir ./datasets/xBD_datasets/xBD_polygons_AB \
+--labels_file ./datasets/xBD_datasets/xBD_polygons_AB_csv/satellite_AB_labels.txt
